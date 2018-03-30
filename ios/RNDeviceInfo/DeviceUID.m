1b1656a0081001a7f11c9b9344e02e3c7484d638
Ivan Wu <ivanchungwu@gmail.com>
2018-03-29 17:38:44 -0700

    Remove uid keychain/default

--- ios/RNDeviceInfo/DeviceUID.m
+++ ios/RNDeviceInfo/DeviceUID.m
@@ -43,8 +43,8 @@
     At last, the UID is persisted if needed to.
  */
 - (NSString *)uid {
-    if (!_uid) _uid = [[self class] valueForKeychainKey:_uidKey service:_uidKey];
-    if (!_uid) _uid = [[self class] valueForUserDefaultsKey:_uidKey];
+    //if (!_uid) _uid = [[self class] valueForKeychainKey:_uidKey service:_uidKey];
+    //if (!_uid) _uid = [[self class] valueForUserDefaultsKey:_uidKey];
     if (!_uid) _uid = [[self class] appleIFV];
     if (!_uid) _uid = [[self class] randomUUID];
     [self save];
